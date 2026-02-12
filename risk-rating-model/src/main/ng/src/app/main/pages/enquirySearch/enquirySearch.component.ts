acc6194457f74aea052d87e7125ecfab7ba371a3
Sajeev.khan@gmail.com <Koopil@5656@>
2026-02-12 15:54:53 +0530

    SME Officer v1

--- risk-rating-model/src/main/ng/src/app/main/pages/enquirySearch/enquirySearch.component.ts
+++ risk-rating-model/src/main/ng/src/app/main/pages/enquirySearch/enquirySearch.component.ts
@@ -72,6 +72,18 @@ export class EnquirySearchComponent implements OnChanges {
                         enquiryApplications.push(new EnquiryApplicationModel(loanApplicationResourceModel));
                     }
                 }
+                else if (this._appService.userDetails.riskDepartment === '04') {
+                    // If the user is from the monitoring assesssment department (03), return applications where application monitoringDepartmentInitiator is himself.
+                    if (loanApplicationResourceModel.loanApplication.sarcDepartmentInitiator === this._appService.userDetails.email) {
+                        enquiryApplications.push(new EnquiryApplicationModel(loanApplicationResourceModel));
+                    }
+                }
+                else if (this._appService.userDetails.riskDepartment === '05') {
+                    // If the user is from the monitoring assesssment department (03), return applications where application monitoringDepartmentInitiator is himself.
+                    if (loanApplicationResourceModel.loanApplication.smeDepartmentInitiator === this._appService.userDetails.email) {
+                        enquiryApplications.push(new EnquiryApplicationModel(loanApplicationResourceModel));
+                    }
+                }
                 else if (this._appService.userDetails.riskDepartment === '12') {
                     // If the user is from the monitoring assesssment department (03), return applications where application monitoringDepartmentInitiator is himself.
                     if (loanApplicationResourceModel.loanApplication.sarcDepartmentInitiator === this._appService.userDetails.email) {
